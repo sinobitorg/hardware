8e8ffc0b020b5cc583c7f791a35c751efef634c6
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-03-03 08:46:52 +0100

    LED-Matrix.cpp : make HT1632C_Write_DAT() preserve its input data array HT1632C_Write_DAT(unsigned char Addr,const unsigned char data[],unsigned char num)
    Signed-off-by: Christoph Maier <cm.hardware.software.elsewhere@gmail.com>

--- Test code/LED-Matrix.cpp
+++ Test code/LED-Matrix.cpp
@@ -20,7 +20,20 @@ DigitalOut HT_RD(P0_22);
 DigitalOut HT_WR(P0_23);
 DigitalOut HT_DAT(P0_21);
 
-static unsigned char show[24] = {0b00011111, 0b10000000, 0b00100000, 0b01000000, 0b01001011, 0b00100000, 0b10000000, 0b00010000, 0b10100010, 0b01010000, 0b10101110, 0b00010000, 0b10000111, 0b01010000, 0b10100100, 0b01010000, 0b10000000, 0b00010000, 0b01001101, 0b00100000, 0b00100000, 0b01000000, 0b00011111, 0b10000000};
+static unsigned char show[24] = {
+  0b00011111, 0b10000000,
+  0b00100000, 0b01000000,
+  0b01001011, 0b00100000,
+  0b10000000, 0b00010000,
+  0b10100010, 0b01010000,
+  0b10101110, 0b00010000,
+  0b10000111, 0b01010000,
+  0b10100100, 0b01010000,
+  0b10000000, 0b00010000,
+  0b01001101, 0b00100000,
+  0b00100000, 0b01000000,
+  0b00011111, 0b10000000
+};
 
 static unsigned char com[12] = {0x00,0x04,0x08,0x0C,0x10,0x14,0x18,0x1C,0x20,0x24,0x28,0x2C};
 
@@ -45,30 +58,31 @@ void HT1632C_Write_CMD(unsigned char cmd)                     //MCU向HT1632c写
     HT_CS=1;
 }
 
-void HT1632C_Write_DAT(unsigned char Addr,unsigned char data[],unsigned char num)
+void HT1632C_Write_DAT(unsigned char Addr,const unsigned char data[],unsigned char num)
 {
+    unsigned char d;
     unsigned char i;
     HT_CS=0;
     HT1632C_Write(0xa0,3);                                    //ID:101
     HT1632C_Write(Addr<<1,7);
 
-    for(i=0; i<8; i++) {
+    for(d=data[2*(num-1)], i=0; i<8; i++) {
         HT_WR=0;
-        if(data[2*(num-1)]&0x80)
+        if(d&0x80)
             HT_DAT=1;
         else
             HT_DAT=0;
-        data[2*(num-1)]<<=1;
+        d<<=1;
         HT_WR=1;
     }
 
-    for(i=0; i<4; i++) {
+    for(d=data[2*num-1], i=0; i<4; i++) {
         HT_WR=0;
-        if(data[2*num-1]&0x80)
+        if(d&0x80)
             HT_DAT=1;
         else
             HT_DAT=0;
-        data[2*num-1]<<=1;
+        d<<=1;
         HT_WR=1;
     }
 
@@ -128,6 +142,5 @@ int main()
             HT1632C_Read_DATA(com[i]);
         }
     }
-
 }
 
